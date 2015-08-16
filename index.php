aab27bdbba5d9885712fa66295c0b52ba673e51f
Fatirah <tieyrah9@gmail.com>
2015-08-16 11:08:01 +0800

    Long text

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 echo "hello git at UTM 2015";
 
 echo "Welcome to git learning";
+
+echo "If you've noticed a bug or simply have an idea that you'd like to see come real, why not work on it? Bug reports and feature requests are typically submitted to the issue tracker on our GitHub repository.";
